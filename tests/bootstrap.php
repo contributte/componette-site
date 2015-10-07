ea457839ead98508a115f50b365ebad7940a47a0
Milan Felix Sulc <rkfelix@gmail.com>
2015-10-07 15:46:52 +0200

    Tests: simple tests

---
+++ tests/bootstrap.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Test bootstrap.
+ */
+if (@!include __DIR__ . '/../vendor/autoload.php') {
+    echo 'Install Nette Tester using `composer update --dev`';
+    exit(1);
+}
+
+// Configure environment
+Tester\Environment::setup();
+date_default_timezone_set('Europe/Prague');
+
+// Create temporary directory
+define('TEMP_DIR', __DIR__ . '/tmp/' . getmypid());
+@mkdir(dirname(TEMP_DIR)); // @ - directory may already exist
+Tester\Helpers::purge(TEMP_DIR);
+
+// Test functions
+function test(\Closure $function)
+{
+    $function();
+}
