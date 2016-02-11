c3fb13a41002da74d4959b62482f67ad98e0a57b
Milan Felix Sulc <rkfelix@gmail.com>
2016-02-11 11:36:28 +0100

    Menu: show only tags which are linked to addons

--- app/modules/front/controls/SideMenu/SideMenu.php
+++ app/modules/front/controls/SideMenu/SideMenu.php
@@ -2,8 +2,10 @@
 
 namespace App\Modules\Front\Controls\SideMenu;
 
+use App\Model\ORM\Tag\Tag;
 use App\Model\ORM\Tag\TagRepository;
 use Nette\Application\UI\Control;
+use Nextras\Orm\Collection\ICollection;
 
 final class SideMenu extends Control
 {
@@ -21,13 +23,29 @@ final class SideMenu extends Control
     }
 
     /**
+     * @param ICollection|Tag[] $tags
+     * @return Tag[]
+     */
+    private function ensure(ICollection $tags)
+    {
+        $ensured = [];
+        foreach ($tags as $tag) {
+            if ($tag->addons->countStored() > 0) {
+                $ensured[] = $tag;
+            }
+        }
+
+        return $ensured;
+    }
+
+    /**
      * RENDER ******************************************************************
      */
 
     public function render()
     {
-        $this->template->categories = $this->tagRepository->findWithHighPriority();
-        $this->template->tags = $this->tagRepository->findWithLowPriority();
+        $this->template->categories = $this->ensure($this->tagRepository->findWithHighPriority());
+        $this->template->tags = $this->ensure($this->tagRepository->findWithLowPriority());
 
         $this->template->setFile(__DIR__ . '/templates/menu.latte');
         $this->template->render();
