64977de264e8cbb397de33f1d8a5881d8fc0b0f4
Milan Felix Sulc <rkfelix@gmail.com>
2016-08-06 16:53:25 +0200

    Feature: added migrations [closes #61 & #62]

---
+++ migrations/run.php
@@ -0,0 +1,26 @@
+<?php
+
+use Nette\DI\Container;
+use Nextras\Dbal\Connection;
+use Nextras\Migrations\Bridges;
+use Nextras\Migrations\Bridges\NextrasDbal\NextrasAdapter;
+use Nextras\Migrations\Controllers\HttpController;
+use Nextras\Migrations\Drivers\MySqlDriver;
+use Nextras\Migrations\Extensions\SqlHandler;
+
+/** @var Container $container */
+$container = require_once __DIR__ . '/../app/bootstrap.php';
+
+$connection = $container->getByType(Connection::class);
+$dbal = new NextrasAdapter($connection);
+$driver = new MySqlDriver($dbal);
+
+$controller = new HttpController($driver);
+
+$baseDir = __DIR__;
+$controller->addGroup('structures', "$baseDir/structures");
+$controller->addGroup('basic-data', "$baseDir/basic-data", ['structures']);
+$controller->addGroup('dummy-data', "$baseDir/dummy-data", ['basic-data']);
+$controller->addExtension('sql', new SqlHandler($driver));
+
+$controller->run();
