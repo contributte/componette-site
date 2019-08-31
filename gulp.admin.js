f08adab4e5580230d2f6f4ccd29c0e4213caa098
Milan Felix Šulc <rkfelix@gmail.com>
2019-08-31 16:40:32 +0200

    NPM: upgrade gulp 3 to gulp 4

--- gulp.admin.js
+++ gulp.admin.js
@@ -7,50 +7,50 @@ const order = require('gulp-order');
 const remoteSrc = require('gulp-remote-src');
 
 module.exports = function () {
-    gulp.task('admin-css', function () {
-        gulp.src('www/assets/admin/css/theme.less')
-            .pipe(less())
-            .pipe(minifyCss({compatibility: 'ie8'}))
-            .pipe(concat('admin.bundle.css'))
-            .pipe(gulp.dest('www/dist'));
-    });
+	gulp.task('admin-css', gulp.series(() => {
+		return gulp.src('www/assets/admin/css/theme.less')
+			.pipe(less())
+			.pipe(minifyCss({ compatibility: 'ie8' }))
+			.pipe(concat('admin.bundle.css'))
+			.pipe(gulp.dest('www/dist'));
+	}));
 
-    gulp.task('admin-css-remote', function () {
-        remoteSrc([
-            'twitter-bootstrap/3.3.6/css/bootstrap.min.css'
-        ], {
-            base: 'https://cdnjs.cloudflare.com/ajax/libs/'
-        })
-            .pipe(minifyCss({compatibility: 'ie8'}))
-            .pipe(concat('admin.external.css'))
-            .pipe(gulp.dest('www/dist/'));
-    });
+	gulp.task('admin-css-remote', gulp.series(() => {
+		return remoteSrc([
+			'twitter-bootstrap/3.3.6/css/bootstrap.min.css'
+		], {
+				base: 'https://cdnjs.cloudflare.com/ajax/libs/'
+			})
+			.pipe(minifyCss({ compatibility: 'ie8' }))
+			.pipe(concat('admin.external.css'))
+			.pipe(gulp.dest('www/dist/'));
+	}));
 
-    gulp.task('admin-js', function () {
-        gulp.src([
-                'www/assets/admin/js/**/*.js',
-                'www/assets/vendor/**/*.js'
-            ])
-            .pipe(order([
-                'nette/nette.forms.js',
-                'nette/nette.ajax.js',
-                'main.js'
-            ]))
-            .pipe(uglify())
-            .pipe(concat('admin.bundle.js'))
-            .pipe(gulp.dest('www/dist'));
-    });
+	gulp.task('admin-js', gulp.series(() => {
+		return gulp.src([
+			'www/assets/admin/js/**/*.js',
+			'www/assets/vendor/**/*.js'
+		])
+			.pipe(order([
+				'nette/nette.forms.js',
+				'nette/nette.ajax.js',
+				'main.js'
+			]))
+			.pipe(uglify())
+			.pipe(concat('admin.bundle.js'))
+			.pipe(gulp.dest('www/dist'));
+	}));
 
-    gulp.task('admin-js-remote', function () {
-        remoteSrc([
-            'jquery/2.2.4/jquery.min.js',
-            'twitter-bootstrap/3.3.6/js/bootstrap.min.js',
-            'chosen/1.4.2/chosen.jquery.min.js'
-        ], {
-            base: 'https://cdnjs.cloudflare.com/ajax/libs/'
-        })
-            .pipe(uglify())
-            .pipe(concat('admin.external.js'))
-            .pipe(gulp.dest('www/dist/'));
-    });
+	gulp.task('admin-js-remote', gulp.series(() => {
+		return remoteSrc([
+			'jquery/2.2.4/jquery.min.js',
+			'twitter-bootstrap/3.3.6/js/bootstrap.min.js',
+			'chosen/1.4.2/chosen.jquery.min.js'
+		], {
+				base: 'https://cdnjs.cloudflare.com/ajax/libs/'
+			})
+			.pipe(uglify())
+			.pipe(concat('admin.external.js'))
+			.pipe(gulp.dest('www/dist/'));
+	}));
 };
--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ front();
 admin();
 
 // Register global tasks
-gulp.task('deploy', [
+gulp.task('deploy', gulp.parallel([
     // Back-end
     'admin-css',
     'admin-css-remote',
@@ -20,13 +20,13 @@ gulp.task('deploy', [
     'front-css-remote',
     'front-js',
     'front-js-remote'
-]);
+]));
 
-gulp.task('watch', function () {
+gulp.task('watch', gulp.series(() => {
     // Back-end
-    gulp.watch(['www/assets/admin/css/**/*.css', 'www/assets/admin/css/**/*.less'], ['admin-css']);
-    gulp.watch(['www/assets/admin/js/**/*.js'], ['admin-js']);
+    gulp.watch(['www/assets/admin/css/**/*.css', 'www/assets/admin/css/**/*.less'], gulp.parallel(['admin-css']));
+    gulp.watch(['www/assets/admin/js/**/*.js'], gulp.parallel(['admin-js']));
     // Front-end
-    gulp.watch(['www/assets/front/css/**/*.css', 'www/assets/front/css/**/*.less'], ['front-css']);
-    gulp.watch(['www/assets/front/js/**/*.js'], ['front-js']);
-});
+    gulp.watch(['www/assets/front/css/**/*.css', 'www/assets/front/css/**/*.less'], gulp.parallel(['front-css']));
+    gulp.watch(['www/assets/front/js/**/*.js'], gulp.parallel(['front-js']));
+}));
