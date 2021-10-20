71b981c6204d2b5b2806b6c9d3d996508505d0f7
Vít Rozsíval <mail@vitrozsival.cz>
2021-10-20 09:58:38 +0200

    Replace hash template string with fullhash

--- webpack/loaders/images.ts
+++ webpack/loaders/images.ts
@@ -8,7 +8,7 @@ export default (): RuleSetRule[] => [
         loader: 'file-loader',
         options: {
           esModule: false,
-          name: '[name].[ext]?[hash]',
+          name: '[name].[ext]?[fullhash]',
         },
       },
       {
